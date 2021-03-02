9806e7a26d1323f7359119eb8b9aacc527dbe298
HuuThang <thanghuuvn@gmail.com>
2021-03-02 10:52:31 +0700

    custome navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet, View, Button} from 'react-native';
+import React, {Component, useState} from 'react';
+import {Text, StyleSheet, View, Button, TextInput, Image} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -8,34 +8,66 @@ function HomeScreen({navigation}) {
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button
-        title="Go to Details"
+        title="Update the title"
+        onPress={() => navigation.setOptions({title: 'Home hihi'})}
+      />
+
+      {/* <Button
+        title="Go to Create Post"
         onPress={() =>
-          navigation.push('Details', {
+          navigation.push('CreatePost', {
             itemId: 86,
           })
         }
-      />
+      /> */}
     </View>
   );
 }
 
 function DetailsScreen({route, navigation}) {
-  console.log(route);
-
   const {itemId} = route.params;
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen {JSON.stringify(itemId)}</Text>
 
-      <Button
+      {/* <Button
         title="Go back"
         onPress={() =>
           navigation.push('Details', {
-            itemId: 100,
+            itemId: Math.floor(Math.random() * 100),
           })
         }
+      /> */}
+
+      <Button title="Go to Home" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function CreatePostScreen({navigation, route}) {
+  const [postText, setPostText] = useState('');
+
+  return (
+    <View>
+      <TextInput
+        multiline
+        value={postText}
+        placeholder="What is on your mind ?"
+        style={{height: 200, padding: 20, backgroundColor: 'white'}}
+        onChangeText={setPostText}
       />
     </View>
+  );
+}
+
+function LogoTitle() {
+  // const uri = {uri: './src/assets/1200px-React-icon.svg.png'};
+  return (
+    <Image
+      style={{width: 50, height: 50}}
+      source={require('./src/assets/1200px-React-icon.svg.png')}
+    />
   );
 }
 
@@ -50,16 +82,31 @@ export default class App extends Component {
             name="Home"
             component={HomeScreen}
             options={{
-              gestureEnabled: false,
+              title: 'Home',
+              headerTitle: (props) => <LogoTitle {...props} />,
+              headerStyle: {},
+              headerTitleStyle: {
+                color: '#fff',
+              },
+              headerRight: () => <Button title="Info" />,
             }}
           />
 
           <Stack.Screen
             name="Details"
             component={DetailsScreen}
+            initialParams={{itemId: 100}}
             options={{
               title: 'Chi tiết',
             }}
+          />
+
+          <Stack.Screen
+            name="CreatePost"
+            component={CreatePostScreen}
+            options={({route}) => ({
+              title: route.params.itemId,
+            })}
           />
         </Stack.Navigator>
       </NavigationContainer>
